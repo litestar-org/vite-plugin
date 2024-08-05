761b0812a91bd47e1c85fba8369820d9b20dd414
Cody Fincher <cody.fincher@gmail.com>
2024-08-05 23:21:19 +0000

    fix: remove extra parameter

--- src/inertia-helpers/index.ts
+++ src/inertia-helpers/index.ts
@@ -17,7 +17,7 @@ export async function resolvePageComponent<T>(
 }
 
 
-export function route(routeName: string,options: { relativeUrl?: boolean } = {}, ...args: any[]): string  {
+export function route(routeName: string, ...args: any[]): string  {
     let url = globalThis.routes[routeName];
     if (!url) {
         console.error(`URL '${routeName}' not found in routes.`);
@@ -69,9 +69,6 @@ export function route(routeName: string,options: { relativeUrl?: boolean } = {},
         return "#";
     }
 
-    if (options.relativeUrl) {
-        return url
-    }
     const fullUrl = new URL(url, window.location.origin);
     return fullUrl.href;
 }
